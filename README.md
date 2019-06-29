5b26b834c2097a6fbf292b22d5e19375824e4067
cvxluo <cvxluo@gmail.com>
2019-06-28 23:14:01 -0700

    Reactify kinda works

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 
-Bot for Discord Hack Week  
+# Reactify
 
-Made by Vex#9042  
+Reactify converts images into a reaction array on Discord.
+
+
+## Usage
+
+Invite the bot [here](https://discordapp.com/api/oauth2/authorize?client_id=593314917499535363&permissions=1074080832&scope=bot)
+
+Warning - bot can be unreliable depending on load
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+Created by Vex#9042
+
+:)
--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
+import urllib.request
+from PIL import Image
+import os
+
 
 EMOJI_SERVER_ID = 594361190272991234
+RESOLUTION = 20
 
 client = discord.Client()
 
@@ -9,6 +14,7 @@ async def on_message(message):
 
     if message.author == client.user:
         return
+
 
     if message.content.startswith('!'):
         command = message.content[1:]
@@ -26,12 +32,79 @@ async def on_message(message):
 
                 emoji = "<:" + str(emojis[0].name) + ":" + str(emojis[0].id) + ">"
                 await message.channel.send(emoji)
+                await message.channel.send(" ")
+
+
+
 
             elif type == "reactify" :
                 if message.attachments :
-                    url = message.attachments
+                    url = message.attachments[0].url
+                    img_name = message.attachments[0].filename
 
-                    print(url)
+                    opener = urllib.request.build_opener()
+                    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+                    urllib.request.install_opener(opener)
+                    urllib.request.urlretrieve(url, "./images/" + img_name)
+
+                    path = "./images/" + img_name
+                    image = Image.open(path)
+
+                    width, height = image.size
+                    rgb_im = image.convert('RGB')
+
+
+                    emoji_server = client.get_guild(EMOJI_SERVER_ID)
+
+                    # Split into boxes
+                    # Each square is scaleX width and scaleY height
+
+                    scaleX = int(width / RESOLUTION)
+                    scaleY = int(height / RESOLUTION)
+
+                    for row in range(RESOLUTION) :
+                        msg = await message.channel.send(row)
+
+                        for column in range(RESOLUTION) :
+                            # row, column is x, y of box
+
+                            aR, aG, aB = 0, 0, 0
+
+                            for x in range(scaleX) :
+                                for y in range(scaleY) :
+                                    r, g, b = rgb_im.getpixel((x + column * scaleX, y + row * scaleY))
+
+                                    aR += (r ** 2)
+                                    aG += (g ** 2)
+                                    aB += (b ** 2)
+
+                            aR /= (scaleX * scaleY)
+                            aG /= (scaleX * scaleY)
+                            aB /= (scaleX * scaleY)
+
+                            aR = int(aR ** 0.5)
+                            aG = int(aG ** 0.5)
+                            aB = int(aB ** 0.5)
+
+
+                            # Now we have the RGB value of the square
+                            # We have to create a custom emoji
+
+                            custom_color = Image.new('RGB', (128, 128), color = (aR, aG, aB))
+                            custom_color.save('custom.png')
+
+                            with open("custom.png", 'rb') as custom:
+                                c = await emoji_server.create_custom_emoji(name="custom" + str(column), image=custom.read())
+                                await msg.add_reaction(c)
+                                await c.delete()
+
+                            os.remove("./custom.png")
+
+                            #await message.channel.send(str(aR) + " " + str(aG) + " " + str(aB))
+
+                    os.remove(path)
+
+
 
                 else :
                     await message.channel.send("Please attach an image to your message!")
